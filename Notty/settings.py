f01b489e4c7b416d0b8c0ad7f7ef4c1d914b574e
WOOJINCHO98 <steamedbunwith@gmail.com>
2022-08-19 21:45:36 +0900

    folder create

--- Notty/settings.py
+++ Notty/settings.py
@@ -15,10 +15,10 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
-STATIC_URL = 'static/'
-STATIC_DIR = os.path.join(BASE_DIR,'static/')
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
